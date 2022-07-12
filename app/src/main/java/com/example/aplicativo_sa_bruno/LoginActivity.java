23d443f993b654276fccf6c866a816e7d47fbc86
GVPastel <fernandorpx27@gmail.com>
2022-07-12 08:57:35 -0300

    Telas quase concluidas, falta pequenos detalhes

---
+++ app/src/main/java/com/example/aplicativo_sa_bruno/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplicativo_sa_bruno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
